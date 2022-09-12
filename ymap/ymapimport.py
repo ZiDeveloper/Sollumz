2090c6c7e16f87c59b4889ded78add0230a4fe98
Fabian Zi <6891250+ZiDeveloper@users.noreply.github.com>
2022-09-12 12:53:16 +0200

    Don't use this branch! Use dev-ymap

--- ymap/ymapimport.py
+++ ymap/ymapimport.py
@@ -2,4 +2,4 @@ from ..cwxml.ymap import *
 
 def import_ymap(import_op, filepath, import_settings):
     ymap_xml: CMapData = YMAP.from_xml_file(filepath)
-    ymap_xml.write_xml("G:/Temp/!main/" + ymap_xml.name + ".ymap.xml")
+    # ymap_xml.write_xml("G:/Temp/!main/" + ymap_xml.name + ".ymap.xml")
